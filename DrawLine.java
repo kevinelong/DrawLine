e9e1498aa4bf93372b5f4b11dfeece5b9658d5d0
Kevin Ernest Long <kevinelong@gmail.com>
2025-06-06 11:57:24 -0700

    wip

--- DrawLine.java
+++ DrawLine.java
@@ -1,8 +1,10 @@
 import java.awt.*;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
 import java.util.Timer;
 import java.util.TimerTask;
 
-class CustomCanvas extends Canvas{
+class CustomCanvas extends Canvas {
     int step = 10;
     int x1 = 10;
     int y1 = 10;
@@ -34,22 +36,41 @@ class CustomTask extends TimerTask{
         canvas.updateStep();
     }
 }
-public class DrawLine {
-
-    public static void main(String[] args) {
+public class DrawLine implements KeyListener  {
+    static CustomCanvas canvas;
+    DrawLine(){
         Frame frame = new Frame("Draw Line Example");
         frame.setSize(300, 200);
         frame.setBackground(Color.BLACK);
-        var canvas = new CustomCanvas();
-        var timer = new Timer();
-        timer.scheduleAtFixedRate(new CustomTask(canvas), 1000, 1000);
+        canvas = new CustomCanvas();
+//        var timer = new Timer();
+//        timer.scheduleAtFixedRate(new CustomTask(canvas), 1000, 1000);
 
         frame.add(canvas);
+        canvas.addKeyListener(this);
+        frame.addKeyListener(this);
         frame.setVisible(true);
 
-//        canvas.updateStep();
-//        canvas.updateStep();
-//        canvas.updateStep();
-//        canvas.updateStep();
     }
+    public static void main(String[] args) {
+        DrawLine app = new DrawLine();
+    }
+
+
+    @Override
+    public void keyTyped(KeyEvent e) {
+        System.out.println("keyTyped" + e);
+    }
+
+    @Override
+    public void keyPressed(KeyEvent e) {
+        System.out.println("keyPressed" + e);
+        canvas.updateStep();
+    }
+
+    @Override
+    public void keyReleased(KeyEvent e) {
+        System.out.println("keyReleased" + e);
+    }
+
 }
